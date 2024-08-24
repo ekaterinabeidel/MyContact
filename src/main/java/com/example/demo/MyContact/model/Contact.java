fcd8853752fe1fa56e6dc8be76764cc213182186
ekaterinabeidel <156689206+ekaterinabeidel@users.noreply.github.com>
2024-08-24 06:00:19 +0200

    update field phones Contact and ContactDTO

--- src/main/java/com/example/demo/MyContact/model/Contact.java
+++ src/main/java/com/example/demo/MyContact/model/Contact.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 
@@ -15,7 +17,7 @@ public class Contact {
     private String name;
     private String fullname;
     private String email;
-    private String phone;
+    private List<String> phones;
     private Long ownerId;
 
 
--- src/main/java/com/example/demo/MyContact/model/ContactDTO.java
+++ src/main/java/com/example/demo/MyContact/model/ContactDTO.java
@@ -3,6 +3,8 @@ package com.example.demo.MyContact.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 
@@ -10,11 +12,11 @@ public class ContactDTO {
     private String name;
     private String fullname;
     private String email;
-    private String phone;
+    private List<String> phones;
     public ContactDTO(Contact contact) {
         this.name = contact.getName();
         this.fullname = contact.getFullname();
         this.email = contact.getEmail();
-        this.phone = contact.getPhone();
+        this.phones = contact.getPhones();
     }
 }
