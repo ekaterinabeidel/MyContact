e2d8a5c85043525c93b9e5ffe071985fb3991412
ekaterinabeidel <156689206+ekaterinabeidel@users.noreply.github.com>
2024-09-01 01:56:18 +0200

    comment files

--- src/main/java/com/example/demo/MyContact/AppConfig.java
+++ src/main/java/com/example/demo/MyContact/AppConfig.java
@@ -1,15 +1,15 @@
-package com.example.demo.MyContact;
-import com.example.demo.MyContact.service.contact.ContactService;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
-@Configuration
-public class AppConfig {
-    @Bean
-    @Primary
-    public ContactService contactService(@Qualifier(value = "databaseContactService") ContactService contactService) {
-        return contactService;
-    }
-}
+//package com.example.demo.MyContact;
+//import com.example.demo.MyContact.service.contact.ContactService;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//
+//@Configuration
+//public class AppConfig {
+//    @Bean
+//    @Primary
+//    public ContactService contactService(@Qualifier(value = "databaseContactService") ContactService contactService) {
+//        return contactService;
+//    }
+//}
