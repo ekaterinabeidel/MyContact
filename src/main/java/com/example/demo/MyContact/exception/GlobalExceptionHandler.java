a1a20451bf18b93bf57fccd08cb195b3ef82b12d
ekaterinabeidel <156689206+ekaterinabeidel@users.noreply.github.com>
2024-09-01 10:33:06 +0200

    update GlobalExceptionHandler

--- src/main/java/com/example/demo/MyContact/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/MyContact/exception/GlobalExceptionHandler.java
@@ -1,7 +1,22 @@
 package com.example.demo.MyContact.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+// TODO какие статусы нужно выбрасывать, если контакт или пользователь не найдены?
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ContactNotFoundException.class)
+    public ResponseEntity<String> handleContactNotFoundException(ContactNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(OwnerNotFoundException.class)
+    public ResponseEntity<String> handleOwnerNotFoundException(OwnerNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
 }
